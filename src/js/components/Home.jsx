a54b23eb72fceb3541ce64c74ad3ee264b589c63
allisonjuliana <162182668+allisonjuliana@users.noreply.github.com>
2025-03-31 00:44:16 +0000

    first commit

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -22,6 +22,7 @@ const Home = () => {
 				love!
 			</p>
 		</div>
+		
 	);
 };
 
--- src/js/main.jsx
+++ src/js/main.jsx
@@ -9,10 +9,12 @@ import "bootstrap"
 import '../styles/index.css'
 
 // components
-import Home from './components/Home';
+// import Home from './components/Home';
+import App from '/src/App.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Home/>
+    {/* <Home/> */}
+    <App />
   </React.StrictMode>,
 )
